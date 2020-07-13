439110117080bf57ec22962422af11fbf40a6970
Bobby Sporman <bobby.sporman@yahoo.com>
2020-07-13 11:20:36 -0400

    updates

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-// var cityName = '';
-// var apiKey = 'f2d38d738c494af392e115cdde527abd';
-// var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-// $.ajax({
-//     url: queryUrl,
-//     method: "GET",
-// }).then(function(response){
-//     console.log(response);
-// })
 
 
+$("#search"). on("click", function(event){
+event.preventDefault();
+var city = $("#city-input").val();
 
-var userChoice = 'new york';
-var currentWeather = ``;
+var apiKey = 'f2d38d738c494af392e115cdde527abd';
 
+var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-function getWeather() {
-    //view current weather conditions for that city
-    var apiKey = 'f2d38d738c494af392e115cdde527abd';
-    userChoice = $("#city-input")
-    currentWeather = `http://api.openweathermap.org/data/2.5/weather?q=${userChoice}&appid=${apiKey}`;
-    $.ajax({
-        url: currentWeather,
-        method: "GET",
-    }).then(function(response){
-      console.log(response);}
-    )}
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+   $("#current").text(JSON.stringify(response));
+  });
 
-    getWeather()
-    
+})
+
